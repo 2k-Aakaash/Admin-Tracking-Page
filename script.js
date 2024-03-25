af3a7df8a3bd6aa9a6c818f49d336d8675335783
2K Aakaash <ruokvin434@gmail.com>
2024-03-25 17:09:08 +0530

    Added Login Page

---
+++ script.js
@@ -0,0 +1,5 @@
+const enterButton = document.getElementById("enterButton")
+
+document.getElementById("enterButton").addEventListener('click', function(){
+    window.location.href = "tracking.html"
+})
