1701e507637c868f1b67e27c1f59dcc897f568a3
2K Aakaash <ruokvin434@gmail.com>
2024-04-09 15:39:28 +0530

    Some Responsive Thing

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const enterButton = document.getElementById("enterButton")
 document.getElementById("enterButton").addEventListener('click', function(){
     window.location.href = "tracking.html"
 })
+
+// JavaScript function to toggle the menu
+function toggleMenu() {
+    var menuIcon = document.querySelector('.mobile-menu-icon');
+    var leftSide = document.querySelector('.left-side');
+    menuIcon.classList.toggle('change');
+    leftSide.classList.toggle('collapsed');
+}
+
